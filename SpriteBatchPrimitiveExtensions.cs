e10a97ff5a0c803cbf3c7fb7911231a1c87de64f
danzel <danzel@localhost.geek.nz>
2013-01-26 22:03:23 +1300

    A version of DrawLine that has a texture

--- SpriteBatchPrimitiveExtensions.cs
+++ SpriteBatchPrimitiveExtensions.cs
@@ -34,6 +34,17 @@ namespace IronXna
 
 			spriteBatch.Draw(_pixel, p, null, color, angle, Vector2.Zero, new Vector2(distance, width), SpriteEffects.None, depth);
 		}
+
+		/// <summary>
+		/// Renders a primitive line object.
+		/// </summary>
+		public static void DrawLine(this SpriteBatch spriteBatch, Texture2D texture, Vector2 p, Vector2 p1, Color color, int width = 1, float depth = 0)
+		{
+			float distance = Vector2.Distance(p, p1);
+			float angle = (float)Math.Atan2(p1.Y - p.Y, p1.X - p.X);
+
+			spriteBatch.Draw(texture, p, null, color, angle, Vector2.Zero, new Vector2(distance, width / (float)texture.Height), SpriteEffects.None, depth);
+		}
 		/*
 
 		/// <summary>
