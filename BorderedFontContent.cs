70bb34e3c22f949cee89cfc8ad1f9c83009bfeba
danzel <danzel@localhost.geek.nz>
2013-04-05 18:56:31 +1300

    Add () to characters

--- BorderedFontContent.cs
+++ BorderedFontContent.cs
@@ -67,7 +67,7 @@ namespace IronXna
 					Enumerable.Range('0', '9' - '0' + 1)
 					.Concat(Enumerable.Range('A', 'Z' - 'A' + 1))
 					.Concat(Enumerable.Range('a', 'z' - 'a' + 1)
-					.Concat(new int[] { ':', '.', ',', '!' })).ToArray();
+					.Concat(new int[] { ':', '.', ',', '!', '(', ')' })).ToArray();
 
 				StringBuilder kerningBuilder = new StringBuilder();
 
