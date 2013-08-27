05c907e0e337e43c59d15c10294e987407527669
David Leaver <danzel@localhost.geek.nz>
2013-08-27 16:32:11 +1200

    Command line tool for building SpriteFonts to .PNG and .txt files for usage by unity

---
+++ CommandLineBuilder/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using IronXna;
+
+namespace CommandLineBuilder
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var def = new XmlBorderedFontDefinition(args[0]);
+			var content = new BorderedFontContent(def);
+
+			string fileName = Path.GetFileNameWithoutExtension(args[0]);
+
+			content.InnerTexture.Save(Path.Combine(args[1], fileName + ".inner.png"));
+			content.BorderedTexture.Save(Path.Combine(args[1], fileName + ".border.png"));
+			File.WriteAllText(Path.Combine(args[1], fileName + ".inner.txt"), content.InnerDefStr);
+			File.WriteAllText(Path.Combine(args[1], fileName + ".border.txt"), content.BorderedDefStr);
+			File.WriteAllText(Path.Combine(args[1], fileName + ".kerning.txt"), content.KerningInfo);
+		}
+	}
+}
