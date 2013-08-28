ef0aa005ba4c7aba243ed85d842ce9447673484d
David Leaver <danzel@localhost.geek.nz>
2013-08-28 20:45:01 +1200

    Skip pre-multiplying alpha on unity

--- ColorExtensions.cs
+++ ColorExtensions.cs
@@ -6,7 +6,11 @@ namespace IronXna
 	{
 		public static Color ApplyAlpha(this Color color, float opacity)
 		{
+#if UNITY
+			return new Color(color, opacity);
+#else
 			return new Color((byte)(color.R * opacity), (byte)(color.G * opacity), (byte)(color.B * opacity), (byte)(color.A * opacity));
+#endif
 		}
 	}
 }
