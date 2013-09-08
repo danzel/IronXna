daa90f48312f48cb05b9765acf7d3ed4c86ab0d3
David Leaver <danzel@localhost.geek.nz>
2013-09-08 22:27:16 +1200

    make external retina optional

--- CommandLineBuilder/Program.cs
+++ CommandLineBuilder/Program.cs
@@ -5,24 +5,29 @@ namespace CommandLineBuilder
 {
 	class Program
 	{
-		static void Main(string[] args)
+		private static void Main(string[] args)
 		{
 			var def = new XmlBorderedFontDefinition(args[0]);
 			var content = new BorderedFontContent(def);
 
 			string fileName = Path.GetFileNameWithoutExtension(args[0]);
 
-			content.InnerTexture.Save(Path.Combine(args[1], fileName + ".inner.png"));
-			content.BorderedTexture.Save(Path.Combine(args[1], fileName + ".border.png"));
-			File.WriteAllText(Path.Combine(args[1], fileName + ".inner.txt"), content.InnerDefStr);
-			File.WriteAllText(Path.Combine(args[1], fileName + ".border.txt"), content.BorderedDefStr);
-			File.WriteAllText(Path.Combine(args[1], fileName + ".kerning.txt"), content.KerningInfo);
-
-			content.RetinaInnerTexture.Save(Path.Combine(args[1], fileName + ".inner@2x.png"));
-			content.RetinaBorderedTexture.Save(Path.Combine(args[1], fileName + ".border@2x.png"));
-			File.WriteAllText(Path.Combine(args[1], fileName + ".inner@2x.txt"), content.RetinaInnerDefStr);
-			File.WriteAllText(Path.Combine(args[1], fileName + ".border@2x.txt"), content.RetinaBorderedDefStr);
-			File.WriteAllText(Path.Combine(args[1], fileName + ".kerning@2x.txt"), content.RetinaKerningInfo);
+			if (args.Length == 2 || args[2] == "normalres")
+			{
+				content.InnerTexture.Save(Path.Combine(args[1], fileName + ".inner.png"));
+				content.BorderedTexture.Save(Path.Combine(args[1], fileName + ".border.png"));
+				File.WriteAllText(Path.Combine(args[1], fileName + ".inner.txt"), content.InnerDefStr);
+				File.WriteAllText(Path.Combine(args[1], fileName + ".border.txt"), content.BorderedDefStr);
+				File.WriteAllText(Path.Combine(args[1], fileName + ".kerning.txt"), content.KerningInfo);
+			}
+			if (args.Length == 2 || args[2] == "retina")
+			{
+				content.RetinaInnerTexture.Save(Path.Combine(args[1], fileName + ".inner@2x.png"));
+				content.RetinaBorderedTexture.Save(Path.Combine(args[1], fileName + ".border@2x.png"));
+				File.WriteAllText(Path.Combine(args[1], fileName + ".inner@2x.txt"), content.RetinaInnerDefStr);
+				File.WriteAllText(Path.Combine(args[1], fileName + ".border@2x.txt"), content.RetinaBorderedDefStr);
+				File.WriteAllText(Path.Combine(args[1], fileName + ".kerning@2x.txt"), content.RetinaKerningInfo);
+			}
 		}
 	}
 }
